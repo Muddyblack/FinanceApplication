8127c1edee267a27fec913d9f81df25359bd69ac
Muddyblack <Muddyblack03@gmail.com>
2022-11-27 13:41:53 +0100

    sql start

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,12 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include ".\\.src\\.libraries\\SQLITE\\sqlite3.h"
+#include <iostream>
+
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <filesystem>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +18,59 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+//button to add expenses
+void MainWindow::on_add_expenses_Button_clicked()
+{
+    //DEC
+    std::string db_path = "./.src/Database";
+    std::string db_file = db_path + "/FinanceDataBase.db";
+
+    QString date = ui->calendarWidget->selectedDate().toString("yyyy.MM.dd");
+    QString year = date.split('.')[0];
+    QString category = ui->expensesCategoryComboBox->currentText();
+    QString price = ui->expensesPriceLine->text().replace(',', '.');
+    QString comment = ui->expensesComment->toPlainText();
+
+
+    //Create Path directory if not existent
+    if(!std::filesystem::is_directory(db_path) || std::filesystem::exists(db_path))
+    {
+        std::filesystem::create_directories(db_path);
+    }
+
+
+
+    //connect to DB
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(QString::fromStdString(db_file));
+
+    //open DB and execute Code
+    db.open();
+    QSqlQuery query(db);
+
+    //Create Table if not existing
+    query.prepare("CREATE TABLE IF NOT EXISTS '" + year + "' "
+                  "(buy_date DATE, "
+                   "category VARCHAR(28), "
+                   "price FLOAT, "
+                   "comment TINYTEXT)");
+    query.exec();
+
+
+    //Add expenses to table
+    query.prepare("INSERT INTO '"+ year + "' (buy_date, category, price, comment) "
+                  "VALUES(:bd, :c, :p, :cc)");
+
+
+    query.bindValue(":bd", date);
+    query.bindValue(":c", category);
+    query.bindValue(":p", price);
+    query.bindValue(":cc", comment);
+
+    query.exec();
+
+    db.close();
 }
 
